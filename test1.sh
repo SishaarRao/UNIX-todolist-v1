79c7dc27802269e9003f8a53e6a0a67de29e0474
Sishaar Rao <sishaar@gmail.com>
2017-03-10 09:15:25 -0500

    init tokenize method + fix where substrings of commands were found as commands

--- test1.sh
+++ test1.sh
@@ -8,27 +8,34 @@
 #    This is a step by step of the 
 #    todolist program
 #######################################
-
-
 KEYWORDS=("init" "list" "add" "remove" "complete")
+
+__tokenize (){
+    count=0
+    for param in $*; do
+	# Check if param is keyword
+	if [[ " ${KEYWORDS[*]} " == *" $param "* ]]
+	then
+	    ((count++))
+	fi
+	# Throw error if too many keywords in param
+	if [[ "$count" -gt "1" ]]
+	then
+	    echo "Error Code 3: Too many keywords in input"
+	    exit 3
+	fi
+    done
+    
+
+}
+
+
+
 echo "Parameter:" $*
 echo "Keywords:" ${KEYWORDS[*]}
 
 # Check if parameters hit a keyword
-count=0
-for param in $*; do
-    # Check if param is keyword
-    if [[ " ${KEYWORDS[*]} " == *"$param"* ]]
-    then
-	((count++))
-    fi
-    # Throw error if too many keywords in param
-    if [[ "$count" -gt "1" ]]
-    then
-	echo "Error Code 3: Too many keywords in input"
-	exit 3
-    fi
-    
-done
+
+__tokenize $*
 
 echo $count
