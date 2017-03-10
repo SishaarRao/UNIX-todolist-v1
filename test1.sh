73a85ec3920f03f4815ffc2053aafe1acd0b4eb5
Sishaar Rao <sishaar@gmail.com>
2017-03-10 09:34:03 -0500

    skeleton structure for methods

--- test1.sh
+++ test1.sh
@@ -11,17 +11,39 @@
 KEYWORDS=("init" "list" "add" "remove" "complete")
 
 __tokenize (){
-    count=""
+    command=""
     for param in $*; do
 	if [[ "$count" != "" ]] && [[ " ${KEYWORDS[*]} " == *" $param "* ]]; then
 	    echo "Error Code 3: Too many keywords in input"
 	    exit 3
 	fi
 	if [[ "$count" == "" ]] && [[ " ${KEYWORDS[*]} " == *" $param "* ]]; then
-	    count="$param"
+	    command="$param"
 	fi
     done
 }
+
+init (){
+    # This is the function for initializing a new list
+}
+
+list (){
+    # This is the function for listing the contents of a list
+}
+
+add (){
+    # This is the function for adding an element to a list
+}
+
+remove (){
+    # This is the function for removing an element from a list
+}
+
+complete (){
+    # This is the function for marking a task complete on a list
+}
+
+
 
 echo "Parameter:" $*
 echo "Keywords:" ${KEYWORDS[*]}
