f40571151c055d4449df69b9f5daaa44aa3b1835
Sishaar Rao <sishaar@gmail.com>
2017-03-09 22:18:59 -0500

    code for checking whether parameter is a keyword

--- test1.sh
+++ test1.sh
@@ -9,28 +9,19 @@
 #    todolist program
 #######################################
 
-function parse(){
-    echo "Hello, World!"
-}
-
-containsElement () {
-    local e
-    for e in "${@:2}"; do [[ "$e" == "$1" ]] && return 0; done
-    return 1
-}
-
-
 
 KEYWORDS=("init" "list" "add" "remove" "complete")
 echo "Parameter:" $*
 echo "Keywords:" ${KEYWORDS[*]}
 
-#echo "$* ${KEYWORDS[*]}" | tr ' ' '\n' | sort -u | tr '\n' ' '
-
-for param in $*
-do
-    containsElement "init" "${KEYWORDS[*]}"
-    echo $?
+# Check if parameters hit a keyword
+for param in $*; do
+    if [[ " ${KEYWORDS[*]} " == *"$param"* ]];
+    then
+	echo "Param Matched:" $param
+    else
+	echo "Param No Match:" $param
+    fi
 done
 
 
